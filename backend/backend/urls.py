c94eae449d1e9d0ca2e5e4f8f2058904c1f1425e
Thanushika Kunananthan <cst20046@std.uwu.ac.lk>
2024-01-25 16:25:55 +0530

    backend merge from thanu

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.urls import path,re_path
+from django.urls import re_path as url
 from django.conf.urls.static import static
+from property import views
 
 from property.views import PropertyView
 from rest_framework import routers
@@ -10,6 +13,10 @@ route = routers.DefaultRouter()
 route.register("", PropertyView, basename='propertyview')
 
 urlpatterns = [
+    re_path(r'^property$',views.propertiesApi),
+    re_path(r'^property/([a-zA-Z0-9]+)$',views.propertiesApi),
+    re_path(r'^users$',views.usersApi),
+    re_path(r'^users/([a-zA-Z0-9]+)$',views.usersApi),
     path('admin/', admin.site.urls),
     path('api/', include(route.urls)),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/property/serializers.py
+++ backend/property/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Property
+from .models import Property,users
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
+        fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = users
         fields = '__all__'
